37caad7261c0336b9e8282ddaac8c7acefd248d4
kuntal <iamkuntal2002@gmail.com>
2023-06-02 20:59:41 +0530

    image to ascii image

---
+++ imageToASCII.py
@@ -0,0 +1,29 @@
+from ascii_magic import AsciiArt, Back
+from PIL import ImageEnhance
+
+# output = AsciiArt.from_image("test_image.jpg")
+# output.to_terminal()
+
+#1st one
+# output.image = ImageEnhance.Brightness(output.image).enhance(0.8)
+# output.to_terminal(columns=100,back=Back.BLUE)
+
+
+#2nd one
+# lion_output = AsciiArt.from_image('lion.jpg')
+# lion_output.to_html_file('lion3.html',columns=200,width_ratio=2.0)
+
+
+#3rd one
+# mana_output = AsciiArt.from_image('mana2.jpg')
+# mana_output.image = ImageEnhance.Brightness(mana_output.image).enhance(1.2)
+# mana_output.to_html_file('mana2.html',columns=300,width_ratio=2.3)
+
+
+#4th one
+grp = AsciiArt.from_image('test.jpg')
+grp.image = ImageEnhance.Brightness(grp.image).enhance(1.3)
+grp.image = ImageEnhance.Sharpness(grp.image).enhance(1.3)
+grp.image = ImageEnhance.Color(grp.image).enhance(1.3)
+
+grp.to_html_file('grp2.html',columns=400,width_ratio=2.2)
